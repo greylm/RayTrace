ed5e8d4ca429b70a82bfb5443525279e98ddd289
Logan Grey <47288605+greylm@users.noreply.github.com>
2022-11-25 10:49:58 -0800

    Update 2D_Thayer_RayTrace.py

--- 2D_Thayer_RayTrace.py
+++ 2D_Thayer_RayTrace.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# modified Python translation of the ray trace function from the module of the same name in the RADIATE Fortran software
+# simplified Python translation of the ray trace function from the module of the same name in the RADIATE Fortran software
 def get_RayTrace2D_Thayer_global(e_outgoing, stat_height, h_lev_all, nr_h_lev_all, start_lev, n):
 
     # conversion factor for radians to degrees
